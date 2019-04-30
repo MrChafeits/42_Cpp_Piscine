a63124fb7ccf258075f2204e986f34c9d793ba8f
Clayton Allen <callen@student.42.us.org>
2019-04-30 14:46:52 -0700

    heck I did pretty darn good

--- day01/ex10/main.cpp
+++ day01/ex10/main.cpp
@@ -2,3 +2,23 @@
 #include <iostream>
 #include <sstream>
 #include <string>
+
+int main(int argc, char **argv)
+{
+	int ret = 0;
+	if (argc == 1) std::cout << std::cin.rdbuf();
+	else {
+		for (int i = 1; i < argc; i++) {
+			std::string foil_nam(argv[i]);
+			std::ifstream ifs(foil_nam);
+			if (ifs) {
+				std::cout << ifs.rdbuf();
+			} else {
+				std::cout << "cato9tails: Error: Unable to open "
+						  << foil_nam
+						  << std::endl;
+			}
+		}
+	}
+	return ret;
+}
